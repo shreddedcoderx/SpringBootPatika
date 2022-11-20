3821fdc8c9afb3c74ccc0190183b3b02cc4c5dba
shreddedcoderx <shreddedcoderx@gmail.com>
2022-11-20 14:25:54 +0300

    Initial commit

---
+++ src/main/java/com/shreddedcoderx/springbootpatika/SpringBootPatikaApplication.java
@@ -0,0 +1,13 @@
+package com.shreddedcoderx.springbootpatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPatikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPatikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreddedcoderx/springbootpatika/SpringBootPatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreddedcoderx.springbootpatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPatikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
