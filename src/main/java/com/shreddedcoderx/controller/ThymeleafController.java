6bf4510c739efad685a0396165d5524c06a48933
shreddedcoderx <shreddedcoderx@gmail.com>
2022-11-22 17:31:12 +0300

    @PathVariable required and optional of path variable.

--- src/main/java/com/shreddedcoderx/controller/ThymeleafController.java
+++ src/main/java/com/shreddedcoderx/controller/ThymeleafController.java
@@ -4,6 +4,7 @@ import com.shreddedcoderx.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -11,6 +12,12 @@ import java.util.List;
 
 @Controller
 public class ThymeleafController {
+
+    // option : root {}
+    @GetMapping({"/", "index"})
+    public String index(){
+        return "index";
+    }
 
     // http://127.0.0.1:8080/thymeleaf1
     @GetMapping("/thymeleaf1")
@@ -37,7 +44,7 @@ public class ThymeleafController {
 
     // model object olarak göndermek.
     @GetMapping("/thymeleaf5")
-    public String getTyhmeleaf4ModelObject(Model model) {
+    public String getTyhmeleaf5ModelObject(Model model) {
         model.addAttribute("key_model1", "text");
         ProductDto productDto = ProductDto.builder()
                 .productId(1L).productName("Magic Keyboard Apple").productPrice(Double.valueOf(2000)).build();
@@ -48,7 +55,7 @@ public class ThymeleafController {
 
     // model object List olarak göndermek.
     @GetMapping("/thymeleaf6")
-    public String getTyhmeleaf4ModelObjectList(Model model) {
+    public String getTyhmeleaf6ModelObjectList(Model model) {
         model.addAttribute("key_model1", "text");
         ProductDto productDto = ProductDto.builder()
                 .productId(1L)
@@ -82,4 +89,19 @@ public class ThymeleafController {
         model.addAttribute("key_model2", productDtoList);
         return "thymeleaf6";
     }
+
+    // http://127.0.0.1:8080/thymeleaf7
+    // @PathVariable
+    @GetMapping({"/thymeleaf7/{id}", "/thymeleaf7/"})
+    public String getTyhmeleaf7ModelObject(Model model, @PathVariable(name = "id", required = false) Long id) {
+
+        if (id != null){
+            model.addAttribute("key_model1", "id : " + id);
+        } else {
+            model.addAttribute("key_model1", "id bulunamadı.");
+        }
+        return "thymeleaf7";
+    }
+
+
 }
